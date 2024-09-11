902930a9ce9a95f7cc7cb25d29d143898bbb0ef1
Aman Kanwal <kanwlaman1212@gmail.com>
2024-09-11 17:43:34 +0530

    sjadh

--- app.js
+++ app.js
@@ -19,10 +19,6 @@ app.set("view engine" , "ejs");
 app.set("views" , path.join(__dirname , "views"))
 
 
-app.listen( 8080, ()=>{
-    console.log("listening ...........")
-});
-
 async function main() {
     try {
         await mongoose.connect(process.env.MONGODB_URL, {
